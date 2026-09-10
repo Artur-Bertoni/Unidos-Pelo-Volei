85d578f78fef9488acac1f600e00090b24d57eb7
Artur Bertoni <artur_b_santos@hotmail.com>
2026-09-10 09:26:25 -0300

    Att versão app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.unidospelovolei"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.3.0"
     }
 
     flavorDimensions += "environment"
